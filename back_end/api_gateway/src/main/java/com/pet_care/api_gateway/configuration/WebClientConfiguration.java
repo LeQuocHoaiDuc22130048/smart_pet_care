e1f8fd83104b878c9e444eb0f9c7ac93d8a9ddd4
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2026-02-11 22:23:02 +0700

    initial front_end for pet_care project

---
+++ back_end/api_gateway/src/main/java/com/pet_care/api_gateway/configuration/WebClientConfiguration.java
@@ -0,0 +1,4 @@
+package com.pet_care.api_gateway.configuration;
+
+public class WebClientConfiguration {
+}
---
+++ front_end/pet_care_smart/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <div className="text-xl">Hello</div>
+        </>
+    );
+}
+
+export default App;
