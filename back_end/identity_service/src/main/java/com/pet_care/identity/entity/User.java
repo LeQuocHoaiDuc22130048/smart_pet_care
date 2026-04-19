74abc423d9420cc05bd2346c3c04a53c0540b4ec
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2026-04-19 16:57:15 +0700

    Update Dockerfile for non-root user creation and add Google OAuth endpoint to security configuration

--- back_end/identity_service/src/main/java/com/pet_care/identity/entity/User.java
+++ back_end/identity_service/src/main/java/com/pet_care/identity/entity/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+import com.pet_care.identity.enums.AuthProvider;
 import jakarta.persistence.*;
 
 import lombok.*;
